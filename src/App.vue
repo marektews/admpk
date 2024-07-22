91614f19334c0a7991a2d8a686d9140b236b4bc6
Marek Tews <marek.tews@gmail.com>
2024-07-22 19:37:35 +0200

    Filtrowanie po turach, nowa kolumna 'Tura'

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ const departments_list = ref([])
 const deletingItem = ref(undefined)
 const search = ref('')
 const timer = ref(null)
+const filter_tura = ref('ALL')
 
 const pk_list = computed(() => {
     return pk_list_filtered.value != undefined ? pk_list_filtered.value : (pk_list_orig.value || [])
@@ -35,15 +36,32 @@ watch(search, (nv) => {
     if(timer.value != null)
         clearTimeout(timer.value)
     timer.value = setTimeout(() => {
-        filtering(nv)
+        filtering(nv, filter_tura.value)
         timer.value = null
     }, 500)
 })
 
-function filtering(pattern) {
+watch(filter_tura, (nv) => {
+    if(timer.value != null)
+        clearTimeout(timer.value)
+    timer.value = setTimeout(() => {
+        filtering(search.value, nv)
+        timer.value = null
+    }, 500)
+})
+
+function filtering(pattern, tura) {
+    let list = []
+
     if(pattern.length) {
         pattern = pattern.toLowerCase()
-        pk_list_filtered.value = pk_list_orig.value.filter((item) => {
+        list = pk_list_orig.value.filter((item) => {
+            if(tura !== "ALL") {
+                let dep_tura = department_tura(item.dep_id)
+                if(dep_tura !== tura)
+                    return false
+            }
+
             if(item.regnum1.toLowerCase().includes(pattern)) return true
             if(item.regnum2?.toLowerCase().includes(pattern)) return true
             if(item.regnum3?.toLowerCase().includes(pattern)) return true
@@ -54,8 +72,29 @@ function filtering(pattern) {
             return false
         })
     }
-    else
-        pk_list_filtered.value = undefined
+    else {
+        list = pk_list_orig.value.filter((item) => {
+            if(tura !== "ALL") {
+                let dep_tura = department_tura(item.dep_id)
+                if(dep_tura !== tura)
+                    return false
+            }
+            return true
+        })
+    } 
+    
+    list.sort((a,b) => {
+        let dep_tura_a = department_tura(a.dep_id)
+        let dep_tura_b = department_tura(b.dep_id)
+        let tmp = dep_tura_a.localeCompare(dep_tura_b)
+        if(tmp !== 0) return tmp
+
+        let zb_name_a = department_name(a.dep_id)
+        let zb_name_b = department_name(b.dep_id)
+        return zb_name_a.localeCompare(zb_name_b)
+    })
+
+    pk_list_filtered.value = list
 }
 
 function load_all_pk() {
@@ -64,7 +103,7 @@ function load_all_pk() {
     .then(d => {
         pk_list_orig.value = d
         if(search.value.length > 0)
-            filtering(search.value)
+            filtering(search.value, filter_tura.value)
     })
     .catch(err => console.error('Load all PK exception:', err))
 }
@@ -72,7 +111,19 @@ function load_all_pk() {
 function department_info(dep_id) {
     let d = departments_list.value.find((item) => item.id === dep_id)
     if(d == undefined) return ""
-    return d.lang + " - " + d.name
+    return `${d.lang} - ${d.name}`
+}
+
+function department_tura(dep_id) {
+    let d = departments_list.value.find((item) => item.id === dep_id)
+    if(d == undefined) return ""
+    return `W${d.tura}`
+}
+
+function department_name(dep_id) {
+    let d = departments_list.value.find((item) => item.id === dep_id)
+    if(d == undefined) return ""
+    return d.name
 }
 
 function onStartDelete(pk) {
@@ -100,6 +151,20 @@ function onDelete(pk) {
             <img src="@/assets/Parking_icon.svg" width="40" height="40" />
             <span class="ms-2 title">Parking Torwar - tryb moderatora</span>
         </div>
+        <div class="tura-radio-group">
+            <div class="form-check">
+                <input v-model="filter_tura" value="ALL" class="form-check-input" type="radio" name="tura" id="tura_all">
+                <label class="form-check-label" for="tura_all">Wszystko</label>
+            </div>
+            <div class="form-check">
+                <input v-model="filter_tura" value="W2" class="form-check-input" type="radio" name="tura" id="tura_w2">
+                <label class="form-check-label" for="tura_w2">W2</label>
+            </div>
+            <div class="form-check">
+                <input v-model="filter_tura" value="W3" class="form-check-input" type="radio" name="tura" id="tura_w3">
+                <label class="form-check-label" for="tura_w3">W3</label>
+            </div>
+        </div>
         <div>
             <div class="input-group ">
                 <span class="input-group-text">
@@ -119,11 +184,12 @@ function onDelete(pk) {
     </header>
 
     <main class="mt-3">
-        <table class="table table-dark table-bordered">
+        <table class="table table-bordered">
             <thead>
                 <tr>
                     <th rowspan="2">#</th>
                     <th rowspan="2">Dział</th>
+                    <th rowspan="2">Tura</th>
                     <th rowspan="2">Nr identyfikatora</th>
                     <th colspan="3">Nr rejestracyjny pojazdu</th>
                     <th rowspan="2">Ops</th>
@@ -148,6 +214,7 @@ function onDelete(pk) {
                 <tr v-for="(pk, index) in pk_list" :key="index">
                     <th>{{ index+1 }}</th>
                     <td>{{ department_info(pk.dep_id) }}</td>
+                    <td>{{ department_tura(pk.dep_id) }}</td>
                     <td>{{ pk.pass_nr }}</td>
                     <template v-if="pk.regnum2?.length">
                         <td>{{ pk.regnum1 }}</td>
@@ -196,5 +263,11 @@ header {
 
 td, th {
     vertical-align: middle;
+}
+
+.tura-radio-group {
+    display: flex;
+    flex-direction: row;
+    gap: 20pt;
 }
 </style>
